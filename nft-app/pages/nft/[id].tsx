7c37d5acc9cddbf95b9bba9d03cb872d6598f2d6
Divyanshu Goyal <divyanshu.goyal@cybergindia.local>
2022-05-14 21:43:25 +0530

    Added Third web authentication

--- nft-app/pages/nft/[id].tsx
+++ nft-app/pages/nft/[id].tsx
@@ -1,6 +1,15 @@
 import React from 'react'
+import { useAddress, useDisconnect, useMetamask } from "@thirdweb-dev/react";
 
 function NFTDropPage() {
+  
+  //Authentication
+  const connectWithMetamask = useMetamask();
+  const address =useAddress();
+  const disconnect = useDisconnect();
+  console.log(address)
+
+  //--
   return (
     <div className="flex h-screen flex-col lg:grid lg:grid-cols-10">
       {/*Left*/}
@@ -21,10 +30,14 @@ function NFTDropPage() {
         {/* Header */}
         <header className="flex items-center justify-between">
           <h1 className="w-52 cursor-pointer text-xl font-extralight sm:w-80">The{' '}<span className="font-extrabold underline decoration-pink-600/50"> AloneShadow </span>{' '} NFT Market Place</h1>
-          <button className="rounded-full bg-rose-400 px-4 py-2 text-xs font-bold text-white lg:px-5 lg:py-3 lg:text-base">
-            Sign In</button>
+          <button className="rounded-full bg-rose-400 px-4 py-2 text-xs font-bold text-white lg:px-5 lg:py-3 
+          lg:text-base" onClick={()=> address ? disconnect():connectWithMetamask()}>
+            {address ? 'Sign Out':'Sign In'}</button>
         </header>
         <hr className="my-2 border " />
+        {address && (
+          <p className="text-center text-sm text-rose-400">You are logged in with wallet {address.substring(0,5)}...{address.substring(address.length-5)}</p>
+        )}
         {/* Body */}
         <div className="mt-10 flex flex-1 flex-col items-center space-y-6 text-center lg:space-y-0 lg:justify-center">
           <img className="w-80 object-cover pb-10 lg:h-40" src="https://links.papareact.com/bdy" alt=""/>
